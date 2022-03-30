e9cff64ea6e15efb4d47b3d096cdde819f8abea3
irenerhmi <irenerhmi@student.telkomuniversity.ac.id>
2022-03-30 16:32:16 +0700

    mengubah judul index

--- pembeli/index.php
+++ pembeli/index.php
@@ -12,7 +12,7 @@ require "../koneksidb.php";
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>HaulHally</title>
+    <title>HaulHallyu Merch</title>
     <meta name="robots" content="noindex, follow" />
     <meta name="description" content="Martup is Multipurpose eCommerce HTML Template, that's perfect for any kind of eCommerce websites such as fashion, furniture and many more.">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
